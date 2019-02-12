1a206121f14d81d2780b6e17b5262e7789bf17db
baoat1ts <bao@at1ts.com>
2019-02-12 17:06:32 +0700

    env file

--- mongochat/server.js
+++ mongochat/server.js
@@ -1,8 +1,9 @@
 const mongo = require('mongodb').MongoClient;
 const client = require('socket.io').listen(4000).sockets;
+require('dotenv').load();
 
 // Connect to mongo
-mongo.connect('mongodb://10.69.59.203/mongochat', function(err, db){
+mongo.connect('mongodb://'+process.env.DB_HOST+'/mongochat', function(err, db){
     if(err){
         throw err;
     }
